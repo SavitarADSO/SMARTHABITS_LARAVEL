10ad861a7c0ea321fcd469fe06560b27123630b5
SavitarADSO <145699111+SavitarADSO@users.noreply.github.com>
2024-03-18 08:01:15 -0500

    Encuesta cambio

--- app/Http/Controllers/EncuestaController.php
+++ app/Http/Controllers/EncuestaController.php
@@ -106,18 +106,7 @@ public function listarEncuesta()
 }
 
 
-
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+  
     /**
      * Store a newly created resource in storage.
      *
